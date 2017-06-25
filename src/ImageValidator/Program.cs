6eee04b2a60e75925586260276457513d35c6ad9
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-06-25 14:57:17 +0500

    Project for docker imagebvalidation was added

---
+++ src/ImageValidator/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ImageValidator
+{
+    using System;
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
