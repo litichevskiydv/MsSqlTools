e7bf27869562248271bd102b405f30f3a97dbf78
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-06-25 15:40:37 +0500

    Code for collation validation was added

--- src/ImageValidator/Program.cs
+++ src/ImageValidator/Program.cs
@@ -2,12 +2,51 @@
 namespace ImageValidator
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Dapper;
+    using JetBrains.Annotations;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Options;
+    using Skeleton.Dapper.ConnectionsFactory;
 
+    [UsedImplicitly]
     static class Program
     {
-        static void Main(string[] args)
+        private static IConfigurationRoot _configuration;
+
+        private static void BuildConfiguration(string[] commandLineArguments)
         {
-            Console.WriteLine("Hello World!");
+            var environmentConfiguration = new ConfigurationBuilder()
+                .AddCommandLine(commandLineArguments)
+                .AddEnvironmentVariables("IMAGEVALIDATOR_")
+                .Build();
+            var environmentName = environmentConfiguration["environment"];
+
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(typeof(Program).GetTypeInfo().Assembly.Location))
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddCommandLine(commandLineArguments)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        static int Main(string[] args)
+        {
+            BuildConfiguration(args);
+
+            var connectionsFactory =
+                new SqlConnectionsFactory(Options.Create(
+                    new SqlConnectionsFactoryOptions {SqlServer = _configuration.GetConnectionString("SqlServer")}));
+            using (var connection = connectionsFactory.Create())
+            {
+                var actualCollation = connection.Query<string>("select collation_name from sys.databases where name = 'tempdb'").Single();
+                if (string.Equals(_configuration.GetValue<string>("ExpectedCollation"), actualCollation, StringComparison.OrdinalIgnoreCase) == false)
+                    return 1;
+            }
+
+            return 0;
         }
     }
 }
